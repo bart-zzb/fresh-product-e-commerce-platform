053e38191d9924941f6bde980eb847b580dc4124
bart_zzb <3533272661@qq.com>
2024-03-16 11:58:13 +0800

    2024-03-16 vant搭建

--- mall-frontend/mall-app-frontend/mall-app/src/views/AboutView.vue
+++ mall-frontend/mall-app-frontend/mall-app/src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <van-date-picker
+        v-model="currentDate"
+        title="选择日期"
+        :min-date="minDate"
+        :max-date="maxDate"
+    />
+
   </div>
 </template>
