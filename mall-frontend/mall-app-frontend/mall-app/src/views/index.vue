7a701833bb1f51322db02a241455109575e9e543
bart_zzb <3533272661@qq.com>
2024-03-16 11:58:02 +0800

    2024-03-16 vant搭建

---
+++ mall-frontend/mall-app-frontend/mall-app/src/views/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <!--搜索框-->
+  <van-search v-model="value" placeholder="请输入搜索关键词" />
+
+  <!--轮播-->
+  <van-swipe :autoplay="3000" lazy-render>
+    <van-swipe-item  v-for="image in images" :key="image">
+      <img :src="image" style="height: 200px"/>
+    </van-swipe-item>
+  </van-swipe>
+  <!--列表-->
+  <van-grid :column-num="3">
+    <van-grid-item v-for="value in 6" :key="value" icon="photo-o" text="文字" />
+  </van-grid>
+  <!--商品列表-->
+  <van-card
+      num="2"
+      price="2.00"
+      desc="描述信息"
+      title="商品标题"
+      thumb="https://fastly.jsdelivr.net/npm/@vant/assets/ipad.jpeg"
+  />
+
+  <van-card
+      num="2"
+      price="2.00"
+      desc="描述信息"
+      title="商品标题"
+      thumb="https://fastly.jsdelivr.net/npm/@vant/assets/ipad.jpeg"
+  />
+  <van-card
+      num="2"
+      price="2.00"
+      desc="描述信息"
+      title="商品标题"
+      thumb="https://fastly.jsdelivr.net/npm/@vant/assets/ipad.jpeg"
+  />
+
+  <!--导航菜单-->
+  <div>
+    <router-view></router-view>
+    <van-tabbar active-color="#ee0a24" inactive-color="#000">
+      <van-tabbar-item icon="wap-home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="apps-o">分类页</van-tabbar-item>
+      <van-tabbar-item icon="shopping-cart-o">购物车</van-tabbar-item>
+      <van-tabbar-item icon="user-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+
+</template>
+
+<script setup>
+const images = [
+  'https://fastly.jsdelivr.net/npm/@vant/assets/apple-1.jpeg',
+  'https://fastly.jsdelivr.net/npm/@vant/assets/apple-2.jpeg',
+];
+</script>
+
+<style scoped>
+
+</style>
--- mall-frontend/mall-app-frontend/mall-app/vue.config.js
+++ mall-frontend/mall-app-frontend/mall-app/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const { VantResolver } = require('@vant/auto-import-resolver');
+const ComponentsPlugin = require('unplugin-vue-components/webpack');
+
+module.exports = {
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      // 当 unplugin-vue-components 版本小于 0.26.0 时，使用以下写法
+      //ComponentsPlugin({ resolvers: [VantResolver()] }),
+      //当 unplugin-vue-components 版本大于等于 0.26.0 时，使用以下写法
+      ComponentsPlugin.default({ resolvers: [VantResolver()] }),
+    ],
+  },
+};
