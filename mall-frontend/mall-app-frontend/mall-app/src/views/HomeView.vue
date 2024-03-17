cd48b78283b1df1291320dca13b7002dc9c76fa7
bart_zzb <3533272661@qq.com>
2024-03-17 10:41:45 +0800

    2024-03-17 vant共有尾部设计

--- mall-frontend/mall-app-frontend/mall-app/src/views/HomeView.vue
+++ mall-frontend/mall-app-frontend/mall-app/src/views/HomeView.vue
@@ -1,7 +1,58 @@
 <template>
-  <router-view/>
+  <view>
+    <router-view/>
+  </view>
+  <!-- 共有的尾部 -->
+  <view>
+    <van-tabbar style="height: 80px;" v-model="active">
+      <van-tabbar-item>
+        <van-icon name="wap-home-o" size="45"/>
+        <div style="font-size: 16px;margin-top: 3px;">首页</div>
+      </van-tabbar-item>
+      <van-tabbar-item>
+        <van-icon name="apps-o" size="45"/>
+        <div style="font-size: 16px;margin-top: 3px;">分类</div>
+      </van-tabbar-item>
+      <van-tabbar-item>
+        <van-icon name="video-o" size="45"/>
+        <div style="font-size: 16px;margin-top: 3px;">直播</div>
+      </van-tabbar-item>
+      <van-tabbar-item>
+        <van-icon name="shopping-cart-o" size="45"/>
+        <div style="font-size: 16px;margin-top: 3px;">购物车</div>
+      </van-tabbar-item>
+      <van-tabbar-item>
+        <van-icon name="manager-o" size="45"/>
+        <div style="font-size: 16px;margin-top: 3px;">我的</div>
+      </van-tabbar-item>
+    </van-tabbar>
+  </view>
+  <van-tabbar style="height: 80px;" v-model="active">
+    <van-tabbar-item>
+      <van-icon name="wap-home-o" size="45"/>
+      <div style="font-size: 16px;margin-top: 3px;">首页</div>
+    </van-tabbar-item>
+    <van-tabbar-item>
+      <van-icon name="apps-o" size="45"/>
+      <div style="font-size: 16px;margin-top: 3px;">分类</div>
+    </van-tabbar-item>
+    <van-tabbar-item>
+      <van-icon name="video-o" size="45"/>
+      <div style="font-size: 16px;margin-top: 3px;">直播</div>
+    </van-tabbar-item>
+    <van-tabbar-item>
+      <van-icon name="shopping-cart-o" size="45"/>
+      <div style="font-size: 16px;margin-top: 3px;">购物车</div>
+    </van-tabbar-item>
+    <van-tabbar-item>
+      <van-icon name="manager-o" size="45"/>
+      <div style="font-size: 16px;margin-top: 3px;">我的</div>
+    </van-tabbar-item>
+  </van-tabbar>
 </template>
 
 <script setup>
+import {ref} from "vue";
 
+const active = ref();
 </script>
