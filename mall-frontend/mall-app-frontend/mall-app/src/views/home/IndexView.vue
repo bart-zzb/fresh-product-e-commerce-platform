dc37f2c557d153c4ad04523cfd9a3ac87863ae5b
bart_zzb <3533272661@qq.com>
2024-03-17 11:34:18 +0800

    2024-03-17 vant 轮播图设计

--- mall-frontend/mall-app-frontend/mall-app/src/views/home/IndexView.vue
+++ mall-frontend/mall-app-frontend/mall-app/src/views/home/IndexView.vue
@@ -1,5 +1,24 @@
 <template>
-  <h1>这是首页展示页面</h1>
+  <van-row>
+    <van-col span="4"></van-col>
+    <van-col span="4">
+      <div style="line-height: 50px;">同城生鲜</div>
+    </van-col>
+    <van-col span="16">
+      <van-search v-model="value" placeholder="请输入搜索关键词"/>
+    </van-col>
+  </van-row>
+  <van-row>
+    <van-col span="24">
+      <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+        <van-swipe-item><img src="/imgs/carsoual1.png" width="390px;"></van-swipe-item>
+        <van-swipe-item><img src="/imgs/carsoual1.png" width="390px"></van-swipe-item>
+        <van-swipe-item><img src="/imgs/carsoual1.png" width="390px"></van-swipe-item>
+        <van-swipe-item><img src="/imgs/carsoual1.png" width="390px"></van-swipe-item>
+      </van-swipe>
+    </van-col>
+  </van-row>
+
 </template>
 
 <script setup>
