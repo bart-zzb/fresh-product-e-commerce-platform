a62a8b563f431c2d2d3362d1384fab8ee7f12f8d
bart_zzb <3533272661@qq.com>
2024-03-25 14:55:26 +0800

    2024-03-25 搭建后端项目整体架构

---
+++ mall/mall-front/src/main/java/cn/tedu/config/MybatisPlusConfig.java
@@ -0,0 +1,28 @@
+package cn.tedu.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Kiven
+ */
+@Configuration
+@MapperScan("cn.tedu.service.dao")
+public class MybatisPlusConfig {
+
+    /**
+     * 配置分页内部拦截器
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new
+                PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
