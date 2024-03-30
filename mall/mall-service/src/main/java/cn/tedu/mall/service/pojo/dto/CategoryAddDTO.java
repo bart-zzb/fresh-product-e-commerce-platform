e37cacef5bbeb8d64f41f6670be97c8c035b3c05
bart_zzb <3533272661@qq.com>
2024-03-30 12:12:29 +0800

    2024-03-30 增加类别功能, 待完善

--- mall/mall-service/src/main/java/cn/tedu/mall/service/pojo/dto/CategoryAddDTO.java
+++ mall/mall-service/src/main/java/cn/tedu/mall/service/pojo/dto/CategoryAddDTO.java
@@ -18,21 +18,9 @@ public class CategoryAddDTO implements Serializable {
     @ApiModelProperty(value = "父类别id",required = true,dataType = "Long")
     Long parentId;
 
-    @NotNull(message = "层级类别不能为空")
-    @ApiModelProperty(value = "父类别id",required = true,dataType = "Integer")
-    Integer level;
-
     @NotBlank(message = "类别名称不能为空")
     @ApiModelProperty(value = "类别名称",required = true,dataType = "String")
     String categoryName;
-
-    @NotNull(message = "排序不能为空")
-    @ApiModelProperty(value = "父类别id",required = true,dataType = "Integer")
-    Integer sort;
-
-    @NotNull(message = "是否为父类不能为空")
-    @ApiModelProperty(value = "父类别id",required = true,dataType = "Integer")
-    Integer isParent;
 
     @NotNull(message = "是否启用不能为空")
     @ApiModelProperty(value = "父类别id",required = true,dataType = "Integer")
