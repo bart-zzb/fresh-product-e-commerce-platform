2409567a8ac58202f892ae1dee94758ceea329c1
bart_zzb <3533272661@qq.com>
2024-04-17 22:14:28 +0800

    2024-04-17 前端拦截器设计

--- mall-frontend/mall-app-frontend/mall-app/src/utils/request.js
+++ mall-frontend/mall-app-frontend/mall-app/src/utils/request.js
@@ -9,38 +9,44 @@ const api = axios.create({
     timeout: 5000 // 超时时间
 })
 // 请求拦截
-// api.interceptors.request.use((config) => {
-//
-//     if(localStorage.getItem("token")){
-//         console.log(localStorage.getItem("token"))
-//         config.headers['Authorization'] = 'Bearer ' + localStorage.getItem("token")
-//
-//     }
-//     return config      // 请求拦截里面的需求可以自己写，有的项目需要配置很多，有的很少，这里就
-//                        // 不放了
-// }, error => {
-//     Promise.reject(error)
-// })
-// // 响应拦截
-// /**
-//  响应拦截也是一样，最基本的可以把返回出来的一些数据做简单处理，
-//  比如把请求回来的提示信息解构出来，给予提示，这样就不用在每次请求完成后单独去写
-//  **/
-// api.interceptors.response.use((res) => {
-//
-//     if(res.data.code === 401 || res.data.code === 403){
-//         console.log("请登录!")
-//         // router.push("/login")
-//     }
-//     if(res.data.code === 200){
-//         showSuccessToast(res.data.message)
-//     }else{
-//         showFailToast(res.data.message)
-//     }
-//
-//     return res.data
-// }, error => {
-//     Promise.reject(error)
-// })
+api.interceptors.request.use((config) => {
+    //配置不拦截的路径
+    let excludePathList = ["mall/carousel/index", "mall/banner/index", "mall/label/**", "mall/live/**","mall/product/**","mall/live_card/**"]
+    console.log(config.url)
+    if(excludePathList.includes(config.url)){
+        console.log("yes")
+        return config;
+    }
+    if(localStorage.getItem("token")){
+        console.log(localStorage.getItem("token"))
+        config.headers['Authorization'] = 'Bearer ' + localStorage.getItem("token")
+
+    }
+    return config      // 请求拦截里面的需求可以自己写，有的项目需要配置很多，有的很少，这里就
+                       // 不放了
+}, error => {
+    Promise.reject(error)
+})
+// 响应拦截
+/**
+ 响应拦截也是一样，最基本的可以把返回出来的一些数据做简单处理，
+ 比如把请求回来的提示信息解构出来，给予提示，这样就不用在每次请求完成后单独去写
+ **/
+api.interceptors.response.use((res) => {
+
+    if(res.data.code === 401 || res.data.code === 403){
+        console.log("请登录!")
+        // router.push("/login")
+    }
+    if(res.data.code === 20000){
+        showSuccessToast(res.data.message)
+    }else{
+        showFailToast(res.data.message)
+    }
+
+    return res.data
+}, error => {
+    Promise.reject(error)
+})
 // 最后导出
 export default api
--- mall-frontend/mall-app-frontend/mall-app/src/views/home/DetailView.vue
+++ mall-frontend/mall-app-frontend/mall-app/src/views/home/DetailView.vue
@@ -20,7 +20,9 @@
       <!--    价格-->
       <van-col span="17">
         <div style="background-color: darkseagreen;color: azure; line-height: 50px;">
-          <b style="font-size: 25px; margin-left: 20px;">{{ content.currentPrice }}</b><s> 零售价：￥{{ content.originalPrice }}</s>
+          <b style="font-size: 25px; margin-left: 20px;">{{ content.currentPrice }}</b><s> 零售价：￥{{
+            content.originalPrice
+          }}</s>
         </div>
       </van-col>
       <van-col span="7">
@@ -88,13 +90,13 @@
       <van-row style="color: darkgrey; font-size: 16px;margin-top: 30px;">
         <van-col :span="6">已选择：</van-col>
         <van-col :span="16">1{{ content.unit }}起售
-          <b style="font-weight: lighter;margin-left: 130px;color: #2c3e50;">{{ num }}份</b></van-col>
+          <b style="font-weight: lighter;margin-left: 130px;color: #2c3e50;">{{ num }}{{ content.unit }}</b></van-col>
         <van-col :span="2">
           <van-cell is-link @click="show = true"/>
         </van-col>
       </van-row>
-      <!-- 选择商品弹出框-->
-      <van-popup v-model:show="show" position="bottom" style="height: 40%;">
+      <!-- 选择购物车弹出框-->
+      <van-popup v-model:show="showCart" position="bottom" style="height: 40%;">
         <van-row style=" margin-left: 15px;">
           <van-col :span="6">
             <img :src="BASE_URL + content.imgUrl" style="width:80px;margin-top: 10px;"></van-col>
@@ -121,15 +123,53 @@
             </tr>
           </table>
         </div>
-      </van-popup>
+        <div style="margin: 15px 20px 0px 20px;">
+          <van-button color="#D54431" style="width: 100%;height: 35px;font-size: 18px;border-radius: 50px;"
+                      @click="addCart()">加入购物车
+          </van-button>
+        </div>
 
+        <!--展示立即购买弹出框-->
+      </van-popup>
+      <van-popup v-model:show="showBuy" position="bottom" style="height: 40%;">
+        <van-row style=" margin-left: 15px;">
+          <van-col :span="6">
+            <img :src="BASE_URL + content.imgUrl" style="width:80px;margin-top: 10px;"></van-col>
+          <van-col :span="18">
+            <p style="margin: 20px 0 20px; font-weight: bold;font-size: 18px;">{{ content.specsName }}</p>
+            <b style="color: #ff2221;font-size: 20px;">￥{{ content.currentPrice }} </b> 库存{{ content.amount }}
+          </van-col>
+        </van-row>
+        <div style="width: 80px;margin: 20px 0 0 15px;">
+          规格<br>
+          <van-button type="danger" style="width: 60px;">{{ content.unit }}</van-button>
+        </div>
+        <div style="width: 80px;margin: 20px 0 0 15px;">
+          数量<br>
+          <table style="width: 100px;">
+            <tr>
+              <td>
+                <button style="font-size: large; width: 25px;" @click="subtract">-</button>
+              </td>
+              <td>{{ num }}</td>
+              <td>
+                <button style="font-size: large; width: 25px;" @click="add">+</button>
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div style="margin: 15px 20px 0px 20px;">
+          <van-button color="#D54431" style="width: 100%;height: 35px;font-size: 18px;border-radius: 50px;">立即购买
+          </van-button>
+        </div>
+      </van-popup>
     </div>
 
     <!--  商品介绍  -->
     <div style="margin-top:30px;height: 30px;line-height: 30px;background-color: darkseagreen;text-align: center;">
       商品介绍
     </div>
-<!--    <img :src="content.detailImgUrl" style="width:395px;">-->
+    <!--    <img :src="content.detailImgUrl" style="width:395px;">-->
 
   </div>
 
@@ -140,8 +180,8 @@
         <van-action-bar-icon icon="chat-o" text="客服"/>
         <van-action-bar-icon icon="cart-o" text="购物车" @click="router.push('/cart')"/>
         <van-action-bar-icon icon="shop-o" text="店铺"/>
-        <van-action-bar-button type="warning" text="加入购物车" @click="show=true"/>
-        <van-action-bar-button type="danger" text="立即购买" @click="show=true"/>
+        <van-action-bar-button type="warning" text="加入购物车" @click="showCart=true"/>
+        <van-action-bar-button type="danger" text="立即购买" @click="showBuy=true"/>
       </van-action-bar>
     </van-tabbar>
   </div>
@@ -153,9 +193,10 @@
 import {onMounted, ref} from "vue";
 import router from "@/router";
 import axios from "@/utils/request";
+import {showToast} from "vant";
 
-const content = ref({imgUrl:''});
-const discountsTime= ref(30 * 60 * 60 * 1000);
+const content = ref({imgUrl: ''});
+const discountsTime = ref(30 * 60 * 60 * 1000);
 // const content = ref({
 //   title: '五花肉 300g/份', imgUrl: '/imgs/detail/detail1.jpg',
 //   newPrice: 13.9, oldPrice: 18.2, goodsId: '0169',
@@ -196,7 +237,8 @@ const onExchange = (code) => {
 };
 
 
-const show = ref(false);
+const showCart = ref(false);
+const showBuy = ref(false);
 
 
 onMounted(() => {
@@ -208,6 +250,21 @@ onMounted(() => {
     }
   })
 })
+
+const addCart = () => {
+  showToast({
+    message: '<div style="font-size: 20px;margin: 20px;">' +
+        '<div style="margin: 10px auto;text-align: center;"><span class="van-icon van-icon-passed" style="color:#13DEA5;"></span></div>' +
+        '<div>加入购物车成功</div></div>',
+    type: 'html',
+    overlay: true,
+    duration: 1500,
+    'close-on-click-overlay': true
+  })
+  setTimeout(() => {
+    showCart.value = false;
+  }, 100);
+}
 
 </script>
 
